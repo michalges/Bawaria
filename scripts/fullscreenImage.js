3240937863b40676a1961dcf5c73e273e319fd4f
michalges <105984108+michalges@users.noreply.github.com>
2023-06-01 22:33:05 +0200

    Delete scripts directory

--- scripts/fullscreenImage.js
+++
@@ -1,7 +0,0 @@
-const fullscreenImageDiv = document.getElementById("fullscreen_image_div");
-const fullscreenImageImg = document.getElementById("fullscreen_image");
-
-function fullscreenImage(el){
-    fullscreenImageDiv.style.display = "flex";
-    console.log(el.src);
-}
